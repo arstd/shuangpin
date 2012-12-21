ef97151860e7c6121591463ff2f3233bb39be85c
Shangxuejin <shangxuejin@gmail.com>
2012-12-21 17:36:46 +0800

    * add hongjq layout

--- public/designassist/idea.js
+++ public/designassist/idea.js
@@ -1,54 +1,43 @@
 /*** fifth ***/
 var idea =  {
-	ei   : "p",	ang  : "y",	iu   : "f",	un   : "g",	uan  : "c",	ou   : "r",
-	ie   : "l",	uang : "l",	sh   : "a",	a    : "a",	zh   : "o",	o    : "o",
-	uo   : "o",	ch   : "e",	e    : "e",	er   : "e",	u    : "u",	i    : "i",
-	ao   : "d",	an   : "h",	iong : "h",	ian  : "t",	ua   : "t",	ong  : "n",
-	in   : "s",	uai  : "s",	ai   : "q",	eng  : "j",	en   : "k",	ia   : "k",
-	ui   : "x",	iang : "x",	iao  : "b",	ing  : "m",	ve   : "w",	v    : "v"
+	ei: "p",	ang : "y",	iu  : "f",	un : "g",	uan: "c",	ou : "r",
+	ie: "l",	uang: "l",	sh  : "a",	a  : "a",	zh : "o",	o  : "o",
+	uo: "o",	ch  : "e",	e   : "e",	er : "e",	u  : "u",	i  : "i",
+	ao: "d",	an  : "h",	iong: "h",	ian: "t",	ua : "t",	ong: "n",
+	in: "s",	uai : "s",	ai  : "q",	eng: "j",	en : "k",	ia : "k",
+	ui: "x",	iang: "x",	iao : "b",	ing: "m",	ve : "w",	v  : "v"
 };
+/**************/
+/*** hongjq ***
+var idea = {
+    a   :"a",   o   :"o",   e   :"e",   i   :"i",   u   :"u",   v   :"k",
+    iang:"p",   uang:"p",   un  :"y",   ia  :"f",   en  :"f",   ou  :"g",
+    ao  :"c",   uan :"r",   er  :"r",   ie  :"l",   uo  :"o",   ch  :"e",
+    zh  :"u",   sh  :"i",   ai  :"d",   ang :"h",   ui  :"t",   ve  :"t",
+    ong :"n",   iong:"n",   an  :"s",   eng :"q",   ing :"j",   ua  :"j",
+    uai :"k",   in  :"x",   iao :"b",   ian :"m",   ei  :"w",   iu  :"v"
+};
+
+***************/
+
 /*** lispathon ***
 [声母] 
-
 # 双拼编码就是它本身的声母不必列出 
-
-ch=V 
-sh=U 
-zh=I 
-
+ch=V sh=U zh=I 
 [韵母] 
-
 # 双拼编码就是它本身的韵母不必列出 
+ai=D an=P ang=H ao=K ei=Z en=T eng=G er=R ia=W ian=N iang=L iao=Y ie=X in=M ing=C iong=S 
+iu=Q ng=G ong=S ou=B ua=W uai=C uan=R uang=L ue=F ui=I un=J uo=O
 
-ai=D 
-an=P 
-ang=H 
-ao=K 
-ei=Z 
-en=T 
-eng=G 
-er=R 
-ia=W 
-ian=N 
-iang=L 
-iao=Y 
-ie=X 
-in=M 
-ing=C 
-iong=S 
-iu=Q 
-ng=G 
-ong=S 
-ou=B 
-ua=W 
-uai=C 
-uan=R 
-uang=L 
-ue=F 
-ui=I 
-un=J 
-uo=O
-**************/
+var idea = {
+    a   : "a",  o   : "o",  e   : "e",  i   : "i",  u   : "u",  v   : "v",
+    ch  : "v",  sh  : "u",  zh  : "i",  ai  : "d",  an  : "p",  ang : "h",
+    ao  : "k",  ei  : "z",  en  : "t",  eng : "g",  er  : "r",  ia  : "w",
+    ian : "n",  iang: "l",  iao : "y",  ie  : "x",  in  : "m",  ing : "c",
+    iong: "s",  iu  : "q",  ong : "s",  ou  : "b",  ua  : "w",  uai : "c",
+    uan : "r",  uang: "l",  ve  : "f",  ui  : "i",  un  : "j",  uo  : "o"
+};
+*/
 
 /*** four ***
 var idea = {
