4460735183fbb76411a3394cce9c85c4c361438b
Shangxuejin <shangxuejin@gmail.com>
2013-01-13 19:39:16 +0800

    add sound and typing area outline

--- public/typing/typing.js
+++ public/typing/typing.js
@@ -1,10 +1,25 @@
-
 var typ = {
+	play: function(sound) {
+		switch(sound) {
+		case 'wrong-sound': 
+			var es = new Audio("/sound/error.wav");
+			es.volume = 0.5;
+			es.play(); 
+			break;
+		case 'right-sound': 
+			var ts = new Audio("/sound/type.wav")
+			ts.volume = 0.5;
+			ts.play();
+			break;
+		default: 
+			break;
+		}
+	},
 	stat: {
 		charCount: $('#charCount'),
 		currentCount: $('#currentCount'),
 		rightCount: $('#rightCount'),
-		errorCount: $('#errorCount'),
+		wrongCount: $('#wrongCount'),
 		backspaceCount: $('#backspaceCount'),			
 		usedTime: $('#usedTime'),
 		speed: $('#speed')
@@ -15,18 +30,29 @@ $(function(){
     $('#commit').on({
 	'click': function(){
 	        $('#typing').html($('#text').val()
-		.replace(/[^\x00-\xff]+\n?/g, '')
-		.replace(/[^\n]/g, function(m){
-	            return '<span>' + m + '</span>';
-	        }).replace(/ *\n*$/,'').replace(/\n\n+/g, '<br><br>').replace(/\n/g, '<br>')
-		.replace(/span/, 'span class="current"'));
+			.replace(/[^\x00-\xff]+\n?/g, '')
+			.replace(/[^\n]/g, function(m){
+			    return '<span>' + m + '</span>';
+			}).replace(/ *\n*$/,'').replace(/\n\n+/g, '<br><br>')
+			.replace(/\n/g, '<br>')
+			.replace(/span/, 'span class="current"'));
 		$('#handler')[0].focus();
-		typ.charCount = $('#typing span, #typing br').length;
+		typ.charCount = $('#typing span').length;
 		typ.current = null;
 		clearInterval(typ.timing);
 	}
     });
-    $('#typing').on({
+
+    $('#handler').on({
+	'blur': function() {
+		cannotTyping();
+	},
+	'focus': function() {
+		canTyping();
+	}
+    });
+
+    $('#typing-area').on({
 	'click': function() {
 		$('#handler')[0].focus();
 	}
@@ -55,10 +81,24 @@ $(function(){
     });
 });
 
+function canTyping() {
+	if ($('#typing .current').length) {
+		$('#typing').removeClass('typing-blur').addClass('typing-focus');
+	}
+}
+
+function cannotTyping() {
+	$('#typing').removeClass('typing-focus').addClass('typing-blur');
+}
+
+
 function backspace() {
 	if (typ.currentCount) {
-		typ.current.removeClass('current right error');
-		typ.current = typ.current.prev().addClass('current');
+		typ.current.removeClass('current right wrong');
+
+		typ.current = typ.current.prev();
+		
+		typ.current.addClass('current');
 
 		typ.backspaceCount++;
 		typ.currentCount--;
@@ -71,45 +111,51 @@ function typing(keyCode){
 	}	
 	typ.currentCount++;
 
-	typ.current.removeClass('current right error');
+	typ.current.removeClass('current right wrong');
 
 
 	var char = String.fromCharCode(keyCode);
 	if(!event.shiftKey) char = char.toLowerCase();
+	
+
 	var requreChar = (typ.current[0].tagName == 'BR' ? ' ' : typ.current.text());
 
 	if (char == requreChar) {
 		typ.current.addClass('right');
+		typ.play('right-sound');
 		typ.rightCount++;
 	} else {
-		typ.current.addClass('error');
-		typ.errorCount++;
+		typ.current.addClass('wrong');
+		typ.play('wrong-sound');
+		typ.wrongCount++;
 	}
 	
-	typ.current = typ.current.next().addClass('current');
+	typ.current = typ.current.next();
+	
 	if (!typ.current.length) {
 		finishTyping();
+	} else {
+		typ.current.addClass('current');
 	}
 }
 
 function startTyping() {
-	typ = {
-		charCount: typ.charCount,
-		stat: typ.stat,
+	$.extend(typ, {
 		stime: new Date(),
 		rightCount: 0,
-		errorCount: 0,
+		wrongCount: 0,
 		backspaceCount: 0,
 		currentCount: 0,
 		current: $('#typing .current')
-	}
+	});
 	typ.timing = setInterval(fillTime, 500);
 }
+
 function fillTime() {
 	typ.stat.charCount.text(typ.charCount),
 	typ.stat.usedTime.text((new Date() - typ.stime)/1000),
 	typ.stat.rightCount.text(typ.rightCount),
-	typ.stat.errorCount.text(typ.errorCount),
+	typ.stat.wrongCount.text(typ.wrongCount),
 	typ.stat.backspaceCount.text(typ.backspaceCount),
 	typ.stat.currentCount.text(typ.currentCount);
 	typ.stat.speed.text(Math.round(typ.currentCount/(new Date() - typ.stime)*60000));
@@ -117,9 +163,10 @@ function fillTime() {
 		$(document).scrollTop(window.innerHeight * 0.7 + $(document).scrollTop());
 	}
 }
+
 function finishTyping() {
 	clearInterval(typ.timing);
 	fillTime();
-	$('#handler').blur();
+	$('#handler')[0].blur();
 	alert('Congratulations: ' + Math.round(typ.currentCount / (new Date() - typ.stime) * 60000)  + 'kms');
 }
