40907fe063fbe5e1d860b1e3b91f33b8ff4ea7e9
Shangxuejin <shangxuejin@gmail.com>
2013-03-19 22:58:20 +0800

    statequivs

--- optimal/statequiv.js
+++ optimal/statequiv.js
@@ -18,15 +18,58 @@ for (var i = 1; i < chars.length; i++) {
             row.push(NaN);
             continue;
         }
+        equivs.sort(function(a, b) {return (+a) - (+b)});
         if (equivs.length === 1) {
             row.push(equivs[0]);
             continue;
+        } else if (equivs.length === 2) {
+            row.push(Math.round((equivs[0] + equivs[1]) / 2));
+            continue;
+        } else if (equivs.length === 3) {
+            if (equivs[2] - equivs[1] > 2 * (equivs[1] - equivs[0])) {
+                row.push(Math.round((equivs[0] + equivs[1]) / 2));
+            } else if (equivs[1] - equivs[0] > 2 * (equivs[2] - equivs[1])) {
+                row.push(Math.round((equivs[2] + equivs[1]) / 2));
+            } else {
+                row.push(equivs[1]);
+            }
+            continue;
         }
+        
+        
+        var lowwer, median, upper, lowhalf, uphalf, lowlimit, uplimit, p = 0, ilowlimit, sum = 0;
         if (equivs.length % 2 === 0) {
-            row.push(Math.round((equivs[equivs.length / 2] + equivs[equivs.length / 2 + 1] ) /2));
+            median = (equivs[equivs.length / 2 - 1] + equivs[equivs.length / 2]) / 2;
+            if (equivs.length / 2 % 2 === 0) {
+                lowwer = (equivs[equivs.length / 4 - 1] + equivs[equivs.length / 4]) / 2;
+                upper =  (equivs[equivs.length / 4 * 3 - 1] + equivs[equivs.length / 4 * 3]) / 2;
+            } else {
+                lowwer = equivs[(equivs.length + 2) / 4 - 1];
+                upper =  equivs[equivs.length / 2 + (equivs.length + 2) / 4 - 1];
+            }
         } else {
-            row.push(Math.round(equivs[(equivs.length + 1) / 2]));
+            median = equivs[(equivs.length - 1) / 2];
+            if ((equivs.length - 1) / 2 % 2 === 0) {
+                lowwer = (equivs[(equivs.length - 1) / 4 - 1] + equivs[(equivs.length - 1) / 4]) / 2;
+                upper =  (equivs[(equivs.length - 1) / 4 * 3] + equivs[(equivs.length - 1) / 4 * 3 + 1]) / 2;
+            } else {
+                lowwer = equivs[((equivs.length - 1) / 2 - 1) / 2];
+                upper =  equivs[(equivs.length - 1) / 2 + 1 + ((equivs.length - 1) / 2 - 1) / 2];
+            }
         }
+        lowhalf = median - lowwer, uphalf = upper - median;
+        if (lowhalf < uphalf) {
+            lowlimit = median - 2 * lowhalf, uplimit = median + 2 * lowhalf;
+        } else {
+            lowlimit = median - 2 * uphalf, uplimit = median + 2 * uphalf;
+        }
+        
+        while((equivs[p++] < lowlimit));
+        ilowlimit = p;
+        while(equivs[p] <= uplimit) {
+            sum += equivs[p++];
+        }
+        row.push(Math.round(sum / (p - ilowlimit)));
     }
     table.push(row);
 }
