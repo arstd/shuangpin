95ad969f511c71d5c01d30a3095994ec4603d765
Shangxuejin <shangxuejin@gmail.com>
2013-05-13 16:38:12 +0800

    init

---
+++ 1/optimal/group.js
@@ -0,0 +1,149 @@
+var togethers = { 
+    iong: 'ong' // 固定 iong 和 ong 在一起
+}; 
+
+function gotFins() {
+    var fs = require('fs');
+    var frequencyData = fs.readFileSync('optimal/frequency.txt','utf8');
+    //console.log(frequencyData);
+    
+    var fins = {};
+    var rows = frequencyData.replace(/^\s+|\s+$/, '').split(/\n/);
+    // 韵母字典
+    for (var i = 1; i < rows.length; i++) {
+        var r = rows[i].split(/\s+/);
+        
+        var init = r[2], fin = r[3];
+        
+        !fins[fin] && (fins[fin]={});
+        fins[fin][init] = +r[4];
+    }
+    return fins;
+}
+
+function gotFinsArray() {
+    var finsArray = [];
+    for (var fin in fins) {
+        
+        // 如果是单韵母，略过
+        if ("m,n,g,r".indexOf(fin) > -1) continue;  
+        
+        if (togethers[fin]) continue;
+        
+        finsArray.push(fin);
+    }
+    finsArray.sort();
+    return finsArray;
+}
+
+function gotConsFin() {
+    var consFin = {};// 可相容的韵母
+    var inits = 'b,p,m,f,d,t,n,l,g,k,h,j,q,x,z,c,s,r,zh,ch,sh,y,w'.split(/,/);
+    for (var i = 0; i < finsArray.length; i++) {
+        var one = finsArray[i];
+        anotherLabel:
+        for (var j = 0; j < i; j++) {
+            var another = finsArray[j];
+            for (var k = 0; k < inits.length; k++) {
+                var init = inits[k];
+                // 如果可以和相同的声母组合，就是不相容的
+                // 跳过这个another韵母，取下一个
+                if (fins[one][init] && fins[another][init]) {
+                    continue anotherLabel;
+                }
+            }
+            consFin[one] || (consFin[one] = {});
+            consFin[one][another] = true;
+        }
+    }
+    return consFin;
+}
+
+
+function consistent(fin, ibasket) {
+    if (!consFin[fin]) return false;
+    
+    // 是否和这个篮子里的韵母相容
+    for (var k = 0; k < baskets[ibasket].length; k++) {
+        // 不相容
+        if (!consFin[fin][baskets[ibasket][k]]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// 把第ifin个韵母依次放到第0~count个篮子里，目前已经用了count个篮子(也是第一个空篮子的索引)
+function group(ifin, count) {
+    if(count > 26) return;
+    
+    if (ifin >= finsArray.length) {
+        output();
+        return;
+    }
+    
+    var fin = finsArray[ifin];
+    for (var cfin in consFin[fin]) {
+        var ibasket = finsBas[cfin];
+        
+        // 相容
+        if (consistent(fin, ibasket)) {
+            var ilen = baskets[ibasket].push(fin);
+            finsBas[fin] = ibasket;
+            
+            group(ifin + 1, count);
+            
+            // 回溯
+            baskets[ibasket].splice(ilen - 1);
+            delete finsBas[fin];
+        }
+    }
+    // 放到新篮子里
+    baskets[count] = [fin];
+    finsBas[fin] = count;
+    
+    group(ifin + 1, count + 1);
+    
+    // 回溯
+    baskets.splice(count, 1);
+    delete finsBas[fin];
+}
+
+function output() {
+    for (var toge in togethers) {
+        baskets[finsBas[togethers[toge]]].push(toge);
+    }
+    var txt = (++counter) + ' ' + baskets.length + ' '
+        + JSON.stringify(baskets).replace(/","/g, '-').replace(/"\],\["/g, ' ').replace(/\[\["|"\]\]/g, '');
+    console.log(txt);
+    for (var tog in togethers) {
+        baskets[finsBas[togethers[tog]]].splice(baskets[finsBas[togethers[tog]]].length - 1);
+    }
+}
+
+function go() {
+    var txt = 'index ngroup';
+    for (var i = 1; i <= 26; i++) {
+        txt += ' ' + i;
+    }
+    console.log(txt);
+    
+    group(0, 0);
+}
+
+var fins = gotFins();
+//console.log(fins);
+var finsArray = gotFinsArray();
+//console.log(JSON.stringify(finsArray).replace(/"/g, ''));
+var consFin = gotConsFin();
+//console.log('>>> consFin ' + JSON.stringify(consFin).replace(/"|:true/g, ''));
+//console.log(consFin);
+
+var baskets = [];
+var finsBas = {};
+
+var counter = 0;
+
+// 开始计算
+go();
---
+++ 1/public/designassist/idea.js
@@ -0,0 +1,209 @@
+/*** init  ********************************************************************
+var idea = {
+    "zh":"u",   "ch":"i",   "sh":"v",
+    a   :"a",   o   :"o",   e   :"e",   i   :"i",   u   :"u",   v   :"v",
+};
+*******************************************************************************/
+
+/******************************
+var idea = {"zh":"u","ch":"i","sh":"v","a":"a","o":"o","e":"e","i":"i","u":"u","uo":"o","ai":"h","an":"l","ang":"n","ao":"z","ei":"q","en":"m","eng":"w","ia":"c","ian":"y","iang":"v","iao":"k","ie":"j","in":"p","ing":";","iu":"g","ong":"s","ou":"r","uan":"f","ue":"d","un":"t","v":"b"}
+;
+******************************/
+
+/*** Flexible Shuangpin Input Method  自由双拼输入法(ColemakShin v7.1) ***********/
+var idea =  {
+    ang :"w",    ei  :"f",    iu  :"p",    uan :"g",    iao :"j",    iang:"l",
+    uang:"l",    zh  :"u",    u   :"u",    ian :"y",    ie  :";",    a   :"a",
+    ong :"r",    iong:"r",    in  :"s",    ui  :"k",    ai  :"t",    ing :"d",
+    uai :"s",    eng :"h",    an  :"n",    e   :"e",    ch  :"i",    i   :"i",
+    o   :"o",    uo  :"o",    ou  :"z",    ao  :"x",    ia  :"c",    ua  :"c",
+    sh  :"v",    v   :"v",    un  :"b",    ue  :"k",    en  :"m"
+};
+/******************************************************************************/
+/****************************************************************6.9**********
+var idea =  {
+    ang :"w",    ei  :"f",    iu  :"p",    uan :"g",    ue  :"j",    iang:"l",
+    uang:"l",    zh  :"u",    u   :"u",    ian :"y",    ie  :";",    a   :"a",
+    ong :"r",    iong:"r",    in  :"s",    ui  :"s",    ai  :"t",    ing :"d",
+    uai :"d",    eng :"h",    an  :"n",    e   :"e",    ch  :"i",    i   :"i",
+    o   :"o",    uo  :"o",    ou  :"z",    iao :"x",    ia  :"c",    ua  :"c",
+    sh  :"v",    v   :"v",    un  :"b",    ao  :"k",    en  :"m"
+};
+******************************************************************************/
+/*****************************************************************************
+var idea =  {
+    iu  :"q",    ang :"w",    ei  :"f",    in  :"p",    uan :"g",    iao :"j",
+    iang:"l",    zh  :"u",    u   :"u",    ian :"y",    ie  :";",    a   :"a",
+    ong :"r",    iong:"r",    ing :"s",    uai :"s",    ai  :"t",    ue  :"d",
+    eng :"h",    an  :"n",    e   :"e",    ch  :"i",    i   :"i",    o   :"o",
+    uo  :"o",    ou  :"z",    ao  :"x",    ia  :"c",    ua  :"c",    sh  :"v",
+    v   :"v",    ui  :"v",    un  :"b",    uang:"k",    en  :"m"
+};
+/******************************************************************************/
+
+/*** Flexible Double Spelling Input Method  自由双拼输入法(ColemakShin 5th) ***
+var idea =  {
+    eng :"w",    ei  :"f",    iu  :"p",    uang:"p",    uan :"g",    iao :"j",
+    ong :"l",    iong:"l",    zh  :"u",    u   :"u",    ian :"y",    a   :"a",
+    ou  :"r",    ing :"s",    ua  :"s",    en  :"t",    ia  :"t",    ui  :"d",
+    iang:"d",    ai  :"h",    an  :"n",    e   :"e",    ch  :"i",    i   :"i",
+    o   :"o",    uo  :"o",    in  :"z",    ie  :"x",    ve  :"c",    sh  :"v",
+    v   :"v",    uai :"v",    un  :"b",    ao  :"k",    ang :"m"
+};
+******************************************************************************/
+
+/*** stat frequency 2 ***
+var idea =  {
+    ie  :"q",	uan :"w",	ei  :"f",	ve  :"p",	iu  :"g",	ua  :"g",
+	iao :"j",	ong :"l",	iong:"l",	zh  :"u",	u   :"u",	ian :"y",
+	a   :"a",	ou  :"r",	ing :"s",	uai :"s",	en  :"t",	ia  :"t",
+	ai  :"h",	an  :"n",	e   :"e",	ch  :"i",	i   :"i",	o   :"o",
+	uo  :"o",	in  :"z",	un  :"x",	eng :"c",	sh  :"v",	v   :"v",
+	ui  :"v",	iang:"b",	uang:"b",	ao  :"k",	ang :"m"
+};
+***********************/
+
+/*** stat optimal 3 ***
+var idea = {
+"zh":"u","ch":"i","sh":"v","a":"a","ai":"k","an":"n","ang":"m","ao":"h","e":"e",
+"ei":"f","en":"t","eng":"c","i":"i","ia":"t","ian":"y","iang":"d","iao":"j",
+"ie":"q","in":"z","ing":"s","iong":"l","iu":"p","o":"o","ong":"l","ou":"r",
+"u":"u","ua":"d","uai":"q","uan":"w","uang":"s","ui":"z","un":"x","uo":"o",
+"v":"v","ve":"b"
+};
+*********************/
+/*** stat optimal 2 ***
+var idea = {
+"zh":"u","ch":"i","sh":"v","a":"a","ai":"k","an":"n","ang":"m","ao":"y",
+"e":"e","ei":"p","en":"t","eng":"f","i":"i","ia":"t","ian":"r","iang":"b","iao":"j",
+"ie":"x","in":"c","ing":"d","iong":"s","iu":"w","o":"o","ong":"s",
+"ou":"z","u":"u","ua":"d","uai":"v","uan":"l","uang":"r","ui":"h","un":"q","uo":"o","v":"v","ve":"h"
+};
+*********************/
+/*** stat frequency ***
+var idea =  {
+    iu  :"q",	eng :"w",	ei  :"f",	uan :"p",	uang:"g",	iang:"j",
+	iao :"l",	zh  :"u",	u   :"u",	ui  :"y",	ve  :"y",	a   :"a",
+	ou  :"r",	ong :"s",	iong:"s",	en  :"t",	ia  :"t",	ing :"d",
+	ai  :"h",	an  :"n",	e   :"e",	ch  :"i",	i   :"i",	o   :"o",
+	uo  :"o",	ie  :"z",	ua  :"z",	in  :"x",	ian :"c",	sh  :"v",
+	v   :"v",	uai :"v",	un  :"b",	ao  :"k",	ang :"m"
+};
+***********************/
+/*** stat optimal 1 ***
+var idea = {
+    "zh":"u","ch":"i","sh":"v","a":"a","ai":"h","an":"n","ang":"k",
+    "ao":"y","e":"e","ei":"f","en":"t","eng":"w","i":"i","ia":"t",
+    "ian":"m","iang":"x","iao":"l","ie":"z","in":"c","ing":"d",
+    "iong":"s","iu":"q","o":"o","ong":"s","ou":"r","u":"u","ua":"z",
+    "uai":"d","uan":"p","uang":"m","ui":"v","un":"b","uo":"o","v":"v",
+    "ve":"j"};
+*********************/
+
+/*** colemak 3rd ***
+var idea =  {
+	iu  :"q",	ui  :"w",	ve  :"w",	ei  :"f",	un  :"p",	ie  :"g",
+	ua  :"g",	iang:"j",	uang:"j",	uan :"l",	zh  :"u",	u   :"u",
+	ang :"y",	a   :"a",	eng :"r",	ai  :"s",	ing :"t",	ian :"d",
+	ao  :"h",	an  :"n",	e   :"e",	ch  :"i",	i   :"i",	o   :"o",
+	uo  :"o",	ia  :"z",	in  :"x",	ou  :"c",	sh  :"v",	v   :"v",
+	uai :"v",	iao :"b",	en  :"k",	ong :"m",	iong:"m"
+};
+******http://shin.herokuapp.com/designassist.html            shangxuejin@gmail.com********/
+
+/*** qwerty xiaohe ***
+
+var idea =  {
+	iu  :"q",	ei  :"w",	e   :"e",	uan :"r",	ve  :"t",
+	un  :"y",	sh  :"u",	u   :"u",	ch  :"i",	i   :"i",	o   :"o",
+	uo  :"o",	ie  :"p",	a   :"a",	ong :"s",	iong:"s",	ai  :"d",
+	en  :"f",	eng :"g",	ang :"h",	an  :"j",	uai :"k",	ing :"k",
+	iang:"l",	uang:"l",	ou  :"z",	ia  :"x",	ua  :"x",	ao  :"c",
+	zh  :"v",	v   :"v",	ui  :"v",	in  :"b",	iao :"n",	ian :"m"
+};
+**************/
+
+/*** colemak 2nd ***
+var idea =  {
+	iu  :"q",	ve  :"w",	ei  :"f",	in  :"p",	ing :"g",	eng :"j",
+	ang :"l",	zh  :"u",	u   :"u",	uan :"y",	a   :"a",	ao  :"r",
+	ai  :"s",	iang:"t",	uang:"t",	ou  :"d",	ong :"h",	iong:"h",
+	ian :"n",	uai :"n",	e   :"e",	ch  :"i",	i   :"i",
+	o   :"o",	uo  :"o",	ie  :"z",	un  :"x",	ia  :"c",	ua  :"c",
+	sh  :"v",	v   :"v",	ui  :"v",	iao :"b",	an  :"k",	en  :"m"
+};
+**************/
+
+/*** colemak 1rt ***
+var idea =  {
+	ia  :"q",	ua  :"q",	un  :"w",	uan :"f",	iao :"p",	iu  :"g",
+	eng :"j",	ong :"l",	iong:"l",	u   :"u",	ao  :"y",	zh  :"a",
+	a   :"a",	ang :"r",	ou  :"s",	ei  :"t",	ian :"d",	uai :"d",
+	ai  :"h",	ing :"n",	ch  :"e",	e   :"e",	i   :"i",
+	o   :"o",	uo  :"o",	ve  :"z",	iang:"x",	uang:"x",	in  :"c",
+	sh  :"v",	v   :"v",	ui  :"v",	ie  :"b",	an  :"k",	en  :"m"
+};
+**************/
+
+/*** fifth ***
+var idea =  {
+	ei  :"p",	ang :"y",	iu  :"f",	un  :"g",	uan :"c",	ou  :"r",
+	ie  :"l",	uang:"l",	sh  :"a",	a   :"a",	zh  :"o",	o   :"o",
+	uo  :"o",	ch  :"e",	e   :"e",	u   :"u",	i   :"i",
+	ao  :"d",	an  :"h",	ian :"t",	ua  :"t",	ong :"n",	iong:"n",
+	in  :"s",	uai :"s",	ai  :"q",	eng :"j",	en  :"k",	ia  :"k",
+	ui  :"x",	iang:"x",	iao :"b",	ing :"m",	ve  :"w",	v   :"v"
+};
+**************/
+/*** hongjq ***
+var idea = {
+    a   :"a",   o   :"o",   e   :"e",   i   :"i",   u   :"u",   v   :"k",
+    iang:"p",   uang:"p",   un  :"y",   ia  :"f",   en  :"f",   ou  :"g",
+    ao  :"c",   uan :"r",   er  :"r",   ie  :"l",   uo  :"o",   ch  :"e",
+    zh  :"u",   sh  :"i",   ai  :"d",   ang :"h",   ui  :"t",   ve  :"t",
+    ong :"n",   iong:"n",   an  :"s",   eng :"q",   ing :"j",   ua  :"j",
+    uai :"k",   in  :"x",   iao :"b",   ian :"m",   ei  :"w",   iu  :"v"
+};
+
+***************/
+
+/*** lispathon ***
+[声母]
+# 双拼编码就是它本身的声母不必列出
+ch=V sh=U zh=I
+[韵母]
+# 双拼编码就是它本身的韵母不必列出
+ai=D an=P ang=H ao=K ei=Z en=T eng=G er=R ia=W ian=N iang=L iao=Y ie=X in=M ing=C iong=S
+iu=Q ng=G ong=S ou=B ua=W uai=C uan=R uang=L ue=F ui=I un=J uo=O
+
+var idea = {
+    a   : "a",  o   : "o",  e   : "e",  i   : "i",  u   : "u",  v   : "v",
+    ch  : "v",  sh  : "u",  zh  : "i",  ai  : "d",  an  : "p",  ang : "h",
+    ao  : "k",  ei  : "z",  en  : "t",  eng : "g",  er  : "r",  ia  : "w",
+    ian : "n",  iang: "l",  iao : "y",  ie  : "x",  in  : "m",  ing : "c",
+    iong: "s",  iu  : "q",  ong : "s",  ou  : "b",  ua  : "w",  uai : "c",
+    uan : "r",  uang: "l",  ve  : "f",  ui  : "i",  un  : "j",  uo  : "o"
+};
+*/
+
+/*** four ***
+var idea = {
+     "eng":"p" ,"ei":"y" ,"un":"f" ,"ui":"g" ,"ve":"g" ,"uan":"c" ,"ou":"r"
+    ,"iang":"l" ,"sh":"a" ,"a":"a" ,"zh":"o" ,"o":"o" ,"uo":"o" ,"ch":"e"
+    ,"e":"e" ,"er":"e" ,"z":"u" ,"u":"u" ,"l":"i" ,"i":"i" ,"ao":"d"
+    ,"an":"h" ,"ang":"t" ,"ong":"n" ,"iong":"n" ,"iao":"s" ,"ai":"q" ,"iu":"j"
+    ,"uai":"j" ,"ie":"k" ,"uang":"k" ,"en":"x" ,"ia":"x" ,"ing":"b" ,"ian":"m"
+    ,"ua":"m" ,"in":"w" ,"v":"v"
+};
+*************/
+
+/*** 田生 ***
+var idea = {
+     "ang":"p" ,"ei":"y" ,"ia":"f" ,"ua":"f" ,"uan":"g" ,"ao":"c" ,"eng":"r"
+    ,"ie":"l" ,"ch":"a" ,"a":"a" ,"sh":"o" ,"o":"o" ,"uo":"o"
+    ,"zh":"e" ,"e":"e" ,"u":"u" ,"er":"u" ,"l":"i" ,"i":"i"
+    ,"uai":"d" ,"ing":"d" ,"ong":"h" ,"iong":"h" ,"ai":"t" ,"en":"n"
+    ,"an":"s" ,"iao":"q" ,"un":"j" ,"ou":"k" ,"uang":"x" ,"iang":"x"
+    ,"ian":"b" ,"in":"m" ,"iu":"w" ,"v":"v" ,"ui":"v" ,"z":"z" ,"ve":"z"
+};
+*************/
